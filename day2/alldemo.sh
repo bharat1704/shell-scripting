91faef06bae45c15e495455fecbdfbfb9aea030f
Bharat Mali <158173770+bharat1704@users.noreply.github.com>
2024-11-04 19:31:55 +0530

    Update alldemo.sh

--- day2/alldemo.sh
+++ day2/alldemo.sh
@@ -2,18 +2,21 @@
 
 #single line comment
 
+#Multiple line comments
 <<comment1
-This is a commnet section
+This is a comment section
 comment1
 
-
+# Assigning value to a variable 
 myName="bharatmali"
-echo "my name is $firstName, and today's date is $(date) "
+echo "my name is $firstName, and today's date is $(date) "  #Print using echo
 
 
 #First way to read the input
 echo "enter your name to print"
-read yourEnteredname
+read yourEnteredname # This takes the input under the variable yourEnteredname 
+
+#Providing multiple values such as two arguments along with entered variable
 echo "your name is $yourEnteredname, argument 1 is: $1, argument 2 is: $2"
 
 
